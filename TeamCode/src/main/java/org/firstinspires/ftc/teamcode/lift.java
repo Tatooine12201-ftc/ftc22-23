816f3176f90377a75474e4bb894d25049bf5ca94
Sz77 <63186233+Sz77@users.noreply.github.com>
2022-09-26 20:18:50 +0300

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lift.java
@@ -13,6 +13,10 @@ public class lift {
     private static final double startingPoint = 1000;
     public ElapsedTime runtime = new ElapsedTime();
     private DcMotor lift = null;
+    private int level = 0;
+    private int[] levels = {0,100,600,800,1000};
+    private boolean isBusy = false;
+    private Pid Pid;
     // private DcMotor lift = null;
 
     public lift(HardwareMap hw) {
@@ -22,6 +26,12 @@ public class lift {
         lift.setDirection(FORWARD);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         stop();
+        Pid = new Pid(0.5,0.5 ,0.5);
+        Pid.setDirection(false);
+        Pid.setMaxIOutput(0.3);
+        Pid.setOutputLimits(1);
+        Pid.setOutputRampRate(0.5);
+        Pid.setSetpointRange(1);
 
     }
 
@@ -46,46 +56,73 @@ public class lift {
 
         }
     }
-
-    public void goToThird() {
-        if (lift.getCurrentPosition() < 5000) {
-            lift.setPower(lift_speed);
-        } else {
-            stop();
-        }
+    public void up(boolean button){
+      if (!isBusy && button && level<4) {
+        level++;
+    }
+      isBusy=button;
     }
 
-    public void goToSecond() {
-        if (lift.getCurrentPosition() > 2500) {
-            lift.setPower(down_speed);
-        } else if (lift.getCurrentPosition() < 2500) {
-            lift.setPower(lift_speed);
-        } else {
-            stop();
-        }
+    public void down(boolean button){
+       if(!isBusy && button && level>0){
+        level--;
+    }
+       isBusy =button;
+    }
+    public void move(){
+        int a = levels[level];
+       lift.setPower(Pid.getOutput(lift.getCurrentPosition(),a));
+
     }
 
-    public void goToFirst() {
-        if (lift.getCurrentPosition() > 0) {
-            lift.setPower(down_speed);
-        } else {
-            stop();
+    //public void goToThird() {
+        //if (lift.getCurrentPosition() < 5000) {
+         //   lift.setPower(lift_speed);
+       // } else {
+        //    stop();
+       // }
+    //}
 
-        }
-    }
+    //public void goToSecond() {
+      //  if (lift.getCurrentPosition() > 2500) {
+       //     lift.setPower(down_speed);
+       // } else if (lift.getCurrentPosition() < 2500) {
+       //     lift.setPower(lift_speed);
+       // } else {
+       //     stop();
+      //  }
+   // }
 
-    public void goToStartingPoint() {
-        if (lift.getCurrentPosition() > startingPoint) {
-            lift.setPower(down_speed);
-        } else if (lift.getCurrentPosition() < startingPoint) {
-            lift.setPower(lift_speed);
-        } else {
-            stop();
-        }
-    }
+   // public void goToFirst() {
+      //  if (lift.getCurrentPosition() > 0) {
+         //   lift.setPower(down_speed);
+       // } else {
+       //     stop();
+
+       // }
+   // }
+
+   // public void goToStartingPoint() {
+      //  if (lift.getCurrentPosition() > startingPoint) {
+        //    lift.setPower(down_speed);
+       // } else if (lift.getCurrentPosition() < startingPoint) {
+        //    lift.setPower(lift_speed);
+      //  } else {
+       //     stop();
+      //  }
+   // }
+   // public void goToFirst(int sec) {
+      //  ElapsedTime runtime  = new ElapsedTime();
+        //runtime.reset();
+       // while ((runtime.time() < sec)) {
+       //     goToFirst();
+       // }
+      //  stop();
+   // }
 
     public void stop() {
         lift.setPower(0);
     }
 }
+
 
