9999fb9b8824a98101c2b8226e5ab30986cdff21
Sz77 <zalmanstefan2004@gmail.com>
2023-03-15 22:32:28 +0200

    new pid system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pid.java
@@ -12,109 +12,255 @@
 //P is main power, I looks at the sum of error and gives final push, D is how much the error is changing
 public class Pid {
     boolean F_togle;
-    //TODO look at using System instead of ElapsedTime
-    private final ElapsedTime elapsedTime;
-    double tolerates = 0;
-    double integral = 0;
-    double derivative = 0;
-    private double maxI = 0;
-    private double previousTime;
-    private double kp;
-    private double ki;
-    private double kd;
-    private double f;
+    private double kP, kI, kD, kF;
+    private double setPoint;
+    private double measuredValue;
+    private double minIntegral, maxIntegral;
 
-    public Pid(double kp, double ki, double kd, double f, boolean f_togle) {
-        this.F_togle = f_togle;
-        elapsedTime = new ElapsedTime();
-        this.previousTime = 0;
-        this.kp = kp;
-        this.ki = ki;
-        this.kd = kd;
-        this.f = f;
-    }
-    public Pid(double kp, double ki, double kd, double f) {
-        elapsedTime = new ElapsedTime();
-        this.previousTime = 0;
-        this.kp = kp;
-        this.ki = ki;
-        this.kd = kd;
-        this.f = f;
+    private double errorVal_p;
+    private double errorVal_v;
+
+    private double totalError;
+    private double prevErrorVal;
+
+    private double errorTolerance_p = 0.05;
+    private double errorTolerance_v = Double.POSITIVE_INFINITY;
+
+    private double lastTimeStamp;
+    private double period;
+
+    /**
+     * The base constructor for the PIDF controller
+     */
+    public Pid(double kp, double ki, double kd, double kf) {
+        this(kp, ki, kd, kf, 0, 0);
     }
 
-    public double getTolerates() {
-        return tolerates;
+    /**
+     * This is the full constructor for the PIDF controller. Our PIDF controller
+     * includes a feed-forward value which is useful for fighting friction and gravity.
+     * Our errorVal represents the return of e(t) and prevErrorVal is the previous error.
+     *
+     * @param sp The setpoint of the pid control loop.
+     * @param pv The measured value of he pid control loop. We want sp = pv, or to the degree
+     *           such that sp - pv, or e(t) < tolerance.
+     */
+    public Pid(double kp, double ki, double kd, double kf, double sp, double pv) {
+        kP = kp;
+        kI = ki;
+        kD = kd;
+        kF = kf;
+
+        setPoint = sp;
+        measuredValue = pv;
+
+        minIntegral = -1.0;
+        maxIntegral = 1.0;
+
+        lastTimeStamp = 0;
+        period = 0;
+
+        errorVal_p = setPoint - measuredValue;
+        reset();
     }
 
-    public void setTolerates(double tolerates) {
-        this.tolerates = tolerates;
+    public void reset() {
+        totalError = 0;
+        prevErrorVal = 0;
+        lastTimeStamp = 0;
     }
 
-    public double getMaxIntegral() {
-        return maxI;
+    /**
+     * Sets the error which is considered tolerable for use with {@link #atSetPoint()}.
+     *
+     * @param positionTolerance Position error which is tolerable.
+     */
+    public void setTolerance(double positionTolerance) {
+        setTolerance(positionTolerance, Double.POSITIVE_INFINITY);
     }
 
-    public void setMaxIntegral(double maxI) {
-        this.maxI = maxI;
+    /**
+     * Sets the error which is considered tolerable for use with {@link #atSetPoint()}.
+     *
+     * @param positionTolerance Position error which is tolerable.
+     * @param velocityTolerance Velocity error which is tolerable.
+     */
+    public void setTolerance(double positionTolerance, double velocityTolerance) {
+        errorTolerance_p = positionTolerance;
+        errorTolerance_v = velocityTolerance;
     }
 
-    public boolean getFt(){
-        return F_togle;
+    /**
+     * Returns the current setpoint of the PIDFController.
+     *
+     * @return The current setpoint.
+     */
+    public double getSetPoint() {
+        return setPoint;
     }
 
-    public void setF_togle(boolean f_togle){
-        this.F_togle =f_togle;
+    /**
+     * Sets the setpoint for the PIDFController
+     *
+     * @param sp The desired setpoint.
+     */
+    public void setSetPoint(double sp) {
+        setPoint = sp;
+        errorVal_p = setPoint - measuredValue;
+        errorVal_v = (errorVal_p - prevErrorVal) / period;
     }
 
-    public double getP() {
-        return kp;
+    /**
+     * Returns true if the error is within the percentage of the total input range, determined by
+     * {@link #setTolerance}.
+     *
+     * @return Whether the error is within the acceptable bounds.
+     */
+    public boolean atSetPoint() {
+        return Math.abs(errorVal_p) < errorTolerance_p
+                && Math.abs(errorVal_v) < errorTolerance_v;
+    }
+
+    /**
+     * @return the PIDF coefficients
+     */
+    public double[] getCoefficients() {
+        return new double[]{kP, kI, kD, kF};
+    }
+
+    /**
+     * @return the positional error e(t)
+     */
+    public double getPositionError() {
+        return errorVal_p;
+    }
+
+    /**
+     * @return the tolerances of the controller
+     */
+    public double[] getTolerance() {
+        return new double[]{errorTolerance_p, errorTolerance_v};
+    }
+
+    /**
+     * @return the velocity error e'(t)
+     */
+    public double getVelocityError() {
+        return errorVal_v;
+    }
+
+    /**
+     * Calculates the next output of the PIDF controller.
+     *
+     * @return the next output using the current measured value via
+     * {@link #calculate(double)}.
+     */
+    public double calculate() {
+        return calculate(measuredValue);
+    }
+
+    /**
+     * Calculates the next output of the PIDF controller.
+     *
+     * @param pv The given measured value.
+     * @param sp The given setpoint.
+     * @return the next output using the given measurd value via
+     * {@link #calculate(double)}.
+     */
+    public double calculate(double pv, double sp) {
+        // set the setpoint to the provided value
+        setSetPoint(sp);
+        return calculate(pv);
+    }
+
+    /**
+     * Calculates the control value, u(t).
+     *
+     * @param pv The current measurement of the process variable.
+     * @return the value produced by u(t).
+     */
+    public double calculate(double pv) {
+        prevErrorVal = errorVal_p;
+
+        double currentTimeStamp = (double) System.nanoTime() / 1E9;
+        if (lastTimeStamp == 0) lastTimeStamp = currentTimeStamp;
+        period = currentTimeStamp - lastTimeStamp;
+        lastTimeStamp = currentTimeStamp;
+
+        if (measuredValue == pv) {
+            errorVal_p = setPoint - measuredValue;
+        } else {
+            errorVal_p = setPoint - pv;
+            measuredValue = pv;
+        }
+
+        if (Math.abs(period) > 1E-6) {
+            errorVal_v = (errorVal_p - prevErrorVal) / period;
+        } else {
+            errorVal_v = 0;
+        }
+
+        /*
+        if total error is the integral from 0 to t of e(t')dt', and
+        e(t) = sp - pv, then the total error, E(t), equals sp*t - pv*t.
+         */
+        totalError += period * (setPoint - measuredValue);
+        totalError = totalError < minIntegral ? minIntegral : Math.min(maxIntegral, totalError);
+
+        // returns u(t)
+        return kP * errorVal_p + kI * totalError + kD * errorVal_v + kF * setPoint;
+    }
+
+    public void setPIDF(double kp, double ki, double kd, double kf) {
+        kP = kp;
+        kI = ki;
+        kD = kd;
+        kF = kf;
+    }
+
+    public void setIntegrationBounds(double integralMin, double integralMax) {
+        minIntegral = integralMin;
+        maxIntegral = integralMax;
+    }
+
+    public void clearTotalError() {
+        totalError = 0;
     }
 
     public void setP(double kp) {
-        this.kp = kp;
-    }
-
-    public double getI() {
-        return ki;
+        kP = kp;
     }
 
     public void setI(double ki) {
-        this.ki = ki;
-    }
-
-    public double getD() {
-        return kd;
+        kI = ki;
     }
 
     public void setD(double kd) {
-        this.kd = kd;
+        kD = kd;
+    }
+
+    public void setF(double kf) {
+        kF = kf;
+    }
+
+    public double getP() {
+        return kP;
+    }
+
+    public double getI() {
+        return kI;
+    }
+
+    public double getD() {
+        return kD;
     }
 
     public double getF() {
-        return f;
+        return kF;
     }
 
-    public void setF(double f) {
-        this.f = f;
+    public double getPeriod() {
+        return period;
     }
 
-    public double calculate(double error) {
-        if (Math.abs(error) <= tolerates) {
-
-            if (F_togle){
-                    return f;
-
-            }
-            return 0;
-        }
-        double currentTime = elapsedTime.milliseconds();
-        double p = kp * error;
-        double i = Range.clip(integral + ki * (error * (currentTime - previousTime)), -maxI, maxI);
-        integral = i;
-        double d = kd * ((error - derivative) / (currentTime - previousTime));
-        derivative = error;
-        previousTime = currentTime;
-
-        return f + p + i + d;
-    }
 }
