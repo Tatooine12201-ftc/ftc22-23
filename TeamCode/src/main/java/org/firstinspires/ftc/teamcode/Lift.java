a5c48b0aec84ea6da9b5811105a3f7cd604d4e48
tatooine12201 <tatooine12201@gmail.com>
2023-08-09 17:51:35 +0300

    john and eitan add lift cgs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Lift {
+    private DcMotor LiftMotor = null;
+
+    public Lift(DcMotor liftMotor) {
+        LiftMotor = liftMotor;
+    }
+
+    public DcMotor getLiftMotor() {
+        return LiftMotor;
+    }
+
+    public void setLiftMotor(DcMotor liftMotor) {
+        LiftMotor = liftMotor;
+    }
 }
