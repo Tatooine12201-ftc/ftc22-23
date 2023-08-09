2adbb925d22393a0f03820bfed68fa02c023d671
tatooine12201 <58682100+tatooine12201@users.noreply.github.com>
2023-08-09 18:30:54 +0300

    add constractor to 4bar, michael

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourBar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourBar.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class FourBar {
+
+    private DcMotor fourbarMotor;
+
+    public FourBar(DcMotor fourbarMotor){
+        fourbarMotor = this.fourbarMotor;
+    }
 }
+
