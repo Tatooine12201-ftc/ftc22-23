d07a400ef69c8050b60bc701b3d8b529819ecb06
tatooine12201 <58682100+tatooine12201@users.noreply.github.com>
2023-08-09 18:25:09 +0300

    yonatan add grepper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Gripper {
+    private Servo grepperServo;
+    private boolean isOpen = true;
+
+    public Gripper(Servo grepperServo) {
+        this.grepperServo = grepperServo;
+    }
+
+    public Servo getGrepperServo() {
+        return grepperServo;
+    }
+
+    public void setGrepperServo(Servo grepperServo) {
+        this.grepperServo = grepperServo;
+    }
+
+    public void open(){
+        grepperServo.setPosition(1);
+        isOpen = true;
+    }
+
+    public void close(){
+        grepperServo.setPosition(0);
+        isOpen = false;
+    }
+
+    public void change(){
+        if(isOpen){
+            close();
+        }
+        else {
+            open();
+        }
+    }
 }
